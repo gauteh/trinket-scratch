bc73c5862c8596305cfeb286fb0224606db5fc14
Gaute Hope <eg@gaute.vetsj.com>
2021-09-25 18:47:55 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-#![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
 
+#[cfg(not(test))]
 extern crate panic_halt;
 
+#[cfg(not(test))]
 #[no_mangle]
-pub extern fn main() {
+pub extern "C" fn main() {
+    _main_()
+}
+
+pub fn _main_() {
+    let i: u8 = 0;
     loop {}
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
 }
